c90720bd1830fe94ee40b8ad6bf51e3a0b885dc0
skyepaper <111369963+skyepaper@users.noreply.github.com>
2024-08-16 18:25:27 +0300

    Create Company.js

---
+++ models/Company.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+const CompanySchema=new Schema({
+    name:{
+        type:String,
+        default:''
+    },   
+    city:{
+        type:String,
+        default:''
+    },   
+    logo:{
+        type:String,
+        default:''
+    }, 
+    link:{
+        type:String,
+        default:''
+    }
+})
+
+const Company=mongoose.model("Company", CompanySchema);
+module.exports=Company;
